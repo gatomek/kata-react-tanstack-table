f97633ff49fa3cda60549a89be40d0501c0b2079
Gatomek <tomasz.galinski@gmail.com>
2026-05-23 19:15:29 +0200

    cr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vite'
+import {type ConfigEnv, defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({command}:ConfigEnv) => {
     return {
-        base: '/kata-react-tanstack-table/',
+        base: command === 'build' ? '/kata-react-tanstack-table/' : '/',
         plugins: [react()],
     }
 })
